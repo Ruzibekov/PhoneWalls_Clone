f70b49b032043753f0f5597955f13252b6417c5d
ruzibekov <ruzibekov44@gmail.com>
2023-09-14 11:52:22 +0500

    Initial commit

---
+++ app/src/main/java/uz/ruzibekov/phonewalls_clone/ui/screens/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package uz.ruzibekov.phonewalls_clone.ui.screens.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavGraph.Default()
+        }
+    }
+}
